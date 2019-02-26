d3fcada942bd328cc4b1a724252d7ff779752682
rolandn <roland.neyrinck@gmail.com>
2019-02-26 19:01:20 +0100

    Ajoutez des fichiers projet.

---
+++ webshop_2019/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webshop_2019.Startup))]
+namespace webshop_2019
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
